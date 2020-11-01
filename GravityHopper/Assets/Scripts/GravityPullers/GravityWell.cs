b07155d0423e0e152549ecbf9d180cf30a2f3505
Ben Partridge <ben.partridge249@gmail.com>
2020-11-02 09:17:41 +1100

    Initial test scene with click to add velocity

---
+++ GravityHopper/Assets/Scripts/GravityPullers/GravityWell.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityWell : MonoBehaviour
+{
+    private bool isBeingClicked = false;
+
+    private void OnMouseDown()
+    {
+        Debug.Log("You clicked me");
+        isBeingClicked = true;
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("You stopped clicking me");
+        isBeingClicked = false;
+    }
+
+    private void Update()
+    {
+        if (isBeingClicked)
+        {
+            movePodTowardsWell();
+        }
+    }
+
+    private void movePodTowardsWell()
+    {
+        var pod = PodInformation.Instance;
+        var rb = pod.GetComponent<Rigidbody>();
+
+        Vector3 directionFromPodToWell = transform.position - pod.transform.position;
+        float distance = Vector3.Distance(transform.position, pod.transform.position);
+
+        rb.velocity = directionFromPodToWell.normalized * distance * distance;
+    }
+}
---
+++ GravityHopper/Assets/Scripts/Pod/PodInformation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PodInformation : MonoBehaviour
+{
+    public static PodInformation Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
