553a8c1ec5a2817c24fb3d1133f12fd4db85521a
Ben Partridge <ben.partridge249@gmail.com>
2020-11-03 08:37:44 +1100

    Add radius to gravity wells

---
+++ GravityHopper/Assets/Scripts/GravityWells/GravityField.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityField : MonoBehaviour
+{
+    /// <summary>
+    /// Coefficient to multiply push force by.
+    /// </summary>
+    [SerializeField]
+    private float pushCoefficient;
+
+    private Rigidbody podRb;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        podRb = other.gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (podRb != null)
+        {
+            var direction = podRb.velocity * -1;
+            var distance = Vector3.Distance(transform.position, podRb.transform.position);
+
+            var force = (direction.normalized * pushCoefficient) / distance;
+
+            podRb.AddForce(force);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        podRb = null;
+    }
+}
--- GravityHopper/Assets/Scripts/GravityWells/GravityWell.cs
+++ GravityHopper/Assets/Scripts/GravityWells/GravityWell.cs
@@ -7,6 +7,9 @@ public abstract class GravityWell : MonoBehaviour
 {
 
     [SerializeField]
+    private float wellRadius = 1.0f;
+
+    [SerializeField]
     /// <summary>
     /// Value to multiply movement by when being pulled.
     /// </summary>
@@ -18,11 +21,20 @@ public abstract class GravityWell : MonoBehaviour
     /// </summary>
     private float velocityClamp;
 
+    /// <summary>
+    /// The time it takes for the pod to ramp up to its max velocity
+    /// after being pulled by the well.
+    /// </summary>
+    [SerializeField]
+    private float rampTime = 0.5f;
+
     private bool isBeingClicked = false;
+    private float clickTime;
 
     private void OnMouseDown()
     {
         isBeingClicked = true;
+        clickTime = Time.time;
     }
 
     private void OnMouseUp()
@@ -35,14 +47,18 @@ public abstract class GravityWell : MonoBehaviour
         if (isBeingClicked)
         {
             var pod = PodInformation.Instance;
-            var rb = pod.GetComponent<Rigidbody>();
-
-            Vector3 directionFromPodToWell = transform.position - pod.transform.position;
             float distance = Vector3.Distance(transform.position, pod.transform.position);
 
-            movePodTowardsWell(rb, directionFromPodToWell, distance);
+            if (distance < wellRadius)
+            {
+                var rb = pod.GetComponent<Rigidbody>();
 
-            rb.velocity = rb.velocity.normalized * clampVelocityComponent(rb.velocity.magnitude);
+                Vector3 directionFromPodToWell = transform.position - pod.transform.position;
+
+                movePodTowardsWell(rb, directionFromPodToWell, distance);
+
+                rb.velocity = rb.velocity.normalized * clampVelocityComponent(rb.velocity.magnitude) - new Vector3();
+            }
         }
     }
 
@@ -56,6 +72,11 @@ public abstract class GravityWell : MonoBehaviour
 
     private float clampVelocityComponent(float velocityComponent)
     {
-        return Mathf.Clamp(velocityComponent, velocityComponent, velocityClamp);
+        return Mathf.Clamp(velocityComponent, velocityComponent, ((Time.time - clickTime) / rampTime) * velocityClamp);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, wellRadius);
     }
 }
