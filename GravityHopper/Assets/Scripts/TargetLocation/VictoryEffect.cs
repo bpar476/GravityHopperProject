d5aeac35cb53d0835371deb27e78adcbfa939592
Ben Partridge <ben.partridge249@gmail.com>
2020-11-05 08:47:55 +1100

    Implement victory effect when reaching target location (also add text mesh pro to project)

---
+++ GravityHopper/Assets/Scripts/TargetLocation/VictoryEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryEffect : MonoBehaviour
+{
+    /// <summary>
+    /// Time in seconds after which the scene will be reloaded when the
+    /// victory effect is called
+    /// </summary>
+    [SerializeField]
+    private float reloadPeriod;
+
+    // TODO: Refactor this to use C# Event system
+    public void OnVictory()
+    {
+        StartCoroutine(ReloadSceneAfterPeriod());
+    }
+
+    private IEnumerator ReloadSceneAfterPeriod()
+    {
+        yield return new WaitForSeconds(reloadPeriod);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ GravityHopper/Assets/Scripts/TargetLocation/WinOnEnter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinOnEnter : MonoBehaviour
+{
+
+    /// <summary>
+    /// Game object which when activated will show a visual effect
+    /// that the challenge has been won
+    /// </summary>
+    [SerializeField]
+    private VictoryEffect winEffect;
+
+    private void Awake()
+    {
+        winEffect.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == PodInformation.Instance.gameObject.name)
+        {
+            winEffect.gameObject.SetActive(true);
+            winEffect.OnVictory();
+        }
+    }
+}
