24251a107c5d6c8758742149d908e0aa6dd2dea0
Ben Partridge <ben.partridge249@gmail.com>
2020-11-10 08:56:04 +1100

    Rename pod orientation script to just be about rotation

---
+++ GravityHopper/Assets/Scripts/Pod/PodRotateWithVelocity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PodRotateWithVelocity : MonoBehaviour
+{
+
+    /// <summary>
+    /// Factor affecting how much the pod rotates
+    /// in relation to its velocity.
+    /// </summary>
+    [SerializeField]
+    private float angularVelocityCoefficient;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateAngularVelocity();
+    }
+
+    private void UpdateAngularVelocity()
+    {
+        rb.angularVelocity = new Vector3(rb.velocity.magnitude * angularVelocityCoefficient, 0, 0);
+    }
+}
