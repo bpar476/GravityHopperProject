d10cde8e3012295b33794fd4add40a4ea7ad7cc0
Ben Partridge <ben.partridge249@gmail.com>
2020-11-16 09:06:31 +1100

    Create shader and material for asteroid walls which oscillates when hit.

---
+++ GravityHopper/Assets/Scripts/AsteroidWalls/EnableRendererOnHit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableRendererOnHit : MonoBehaviour
+{
+    [SerializeField]
+    private float period;
+    private Renderer myRenderer;
+
+    private void Awake()
+    {
+        myRenderer = GetComponent<Renderer>();
+        myRenderer.enabled = false;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            myRenderer.enabled = true;
+            StartCoroutine(DisableRendererAfterPeriod());
+        }
+    }
+
+    private IEnumerator DisableRendererAfterPeriod()
+    {
+        yield return new WaitForSecondsRealtime(period);
+        myRenderer.enabled = false;
+    }
+}
