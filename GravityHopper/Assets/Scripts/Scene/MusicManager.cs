387fd03c87178a7ab09ca780a9ac98408bc0cde4
Ben Partridge <ben.partridge249@gmail.com>
2020-11-17 17:39:55 +1100

    Add game music

---
+++ GravityHopper/Assets/Scripts/Scene/MusicManager.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    // Currently does nothing but should be singleton when introducing scene changes
+}
