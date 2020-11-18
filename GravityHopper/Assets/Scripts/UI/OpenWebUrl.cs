696632712063474413ce718c7dfb8540bcddd93e
Ben Partridge <ben.partridge249@gmail.com>
2020-11-19 08:48:16 +1100

    Create basic main menu with play button and socials

---
+++ GravityHopper/Assets/Scripts/UI/OpenWebUrl.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class OpenWebUrl : MonoBehaviour
+{
+    public void OpenUrl(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
---
+++ GravityHopper/Assets/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadGravityHopperScene(int sceneBuildIndex)
+    {
+        SceneManager.LoadScene(sceneBuildIndex);
+    }
+
+    public void LoadGravityHopperSceneAsync(int sceneBuildIndex)
+    {
+        SceneManager.LoadSceneAsync(sceneBuildIndex);
+    }
+}
