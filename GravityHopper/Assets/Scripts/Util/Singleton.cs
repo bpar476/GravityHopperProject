b40903353ed8c00447c30bee3b5ac759b1a6d44e
Ben Partridge <ben.partridge249@gmail.com>
2020-11-23 08:04:49 +1100

    Implement generic singleton base class

---
+++ GravityHopper/Assets/Scripts/Util/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance { get { return instance; } }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            GameObject.Destroy(this);
+        }
+        else
+        {
+            instance = Init();
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    protected abstract T Init();
+}
