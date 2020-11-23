eec78c8ae94035b3c66f09242839b3ae094c7645
Ben Partridge <ben.partridge249@gmail.com>
2020-11-24 08:32:55 +1100

    Implement pausing of Pod while menu is open

---
+++ GravityHopper/Assets/Scripts/Pod/PodPause.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PodPause : MonoBehaviour
+{
+    private Vector3 storedVelocity;
+    private Vector3 storedAngularVelocity;
+
+    private void Start()
+    {
+        var rb = GetComponent<Rigidbody>();
+
+        PauseManager.Instance.OnPause += () =>
+        {
+            storedVelocity = rb.velocity;
+            storedAngularVelocity = rb.angularVelocity;
+            rb.isKinematic = true;
+        };
+
+        PauseManager.Instance.OnResume += () =>
+        {
+            rb.isKinematic = false;
+            rb.velocity = storedVelocity;
+            rb.angularVelocity = storedAngularVelocity;
+        };
+    }
+}
