58c4d35bc2a8da88a0e5ac88d29b903c23597f73
Ben Partridge <ben.partridge249@gmail.com>
2020-11-27 08:19:53 +1100

    Add editor script for deleting player prefs

---
+++ GravityHopper/Assets/Editor/DeletePlayerPrefs.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class DeletePlayerPrefs : EditorWindow
+{
+    [MenuItem("Window/Delete PlayerPrefs (All)")]
+    static void DeleteAllPlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
